765b389dd77594006454afe0aa1bb6bead8e68db
Riya Verma <riyav0711@gmail.com>
2025-09-22 09:52:55 +0000

    i deleted the db from mongodb . re attempt the creation

--- src/models/User.ts
+++ src/models/User.ts
@@ -15,20 +15,22 @@ const UserSchema = new Schema({
   passwordHash: { type: String, required: true, select: false },
   createdAt: { type: Date, default: Date.now },
 }, {
-  // This transform is used for toJSON and toObject
-  transform: (doc, ret) => {
-    ret.id = ret._id.toString();
-    delete ret._id;
-    delete ret.__v;
-    // The passwordHash is NOT deleted here. It is handled by `select: false` in the schema.
+  toJSON: {
+    virtuals: true,
+    transform: (doc, ret) => {
+      delete ret._id;
+      delete ret.__v;
+      // The passwordHash is not present here due to `select: false`
+    }
   },
-  virtuals: true,
+  toObject: {
+    virtuals: true,
+    transform: (doc, ret) => {
+      delete ret._id;
+      delete ret.__v;
+    }
+  }
 });
-
-// Apply the transform to both toJSON and toObject
-UserSchema.set('toJSON', { transform: UserSchema.get('transform') });
-UserSchema.set('toObject', { transform: UserSchema.get('transform') });
-
 
 const UserModel = models.User || model<UserDocument>('User', UserSchema);
 
